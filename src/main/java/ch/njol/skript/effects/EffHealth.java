1b4c298c6dd55e0523018de42d622153b726592e
rudde <hazar.akbuz@outlook.com>
2024-05-05 14:08:39 +0300

    Revert "Fix weird item mutation bug with EffHealth (#6586)"
    This reverts commit cf199c09b047d4234f15e252c65fc0c701cab7fb.

--- src/main/java/ch/njol/skript/effects/EffHealth.java
+++ src/main/java/ch/njol/skript/effects/EffHealth.java
@@ -22,6 +22,8 @@ import ch.njol.skript.Skript;
 import ch.njol.skript.aliases.ItemType;
 import ch.njol.skript.bukkitutil.HealthUtils;
 import ch.njol.skript.bukkitutil.ItemUtils;
+import ch.njol.skript.classes.Changer.ChangeMode;
+import ch.njol.skript.classes.Changer.ChangerUtils;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
@@ -29,112 +31,105 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
-import ch.njol.skript.util.slot.Slot;
 import ch.njol.util.Kleenean;
 import ch.njol.util.Math2;
-import org.bukkit.entity.Damageable;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.ItemStack;
 import org.eclipse.jdt.annotation.Nullable;
 
+/**
+ * @author Peter Güttinger
+ */
 @Name("Damage/Heal/Repair")
 @Description("Damage/Heal/Repair an entity, or item.")
-@Examples({
-	"damage player by 5 hearts",
-	"heal the player",
-	"repair tool of player"
-})
+@Examples({"damage player by 5 hearts",
+		"heal the player",
+		"repair tool of player"})
 @Since("1.0")
 public class EffHealth extends Effect {
 
 	static {
 		Skript.registerEffect(EffHealth.class,
-			"damage %livingentities/itemtypes/slots% by %number% [heart[s]] [with fake cause %-damagecause%]",
+			"damage %livingentities/itemtypes% by %number% [heart[s]] [with fake cause %-damagecause%]",
 			"heal %livingentities% [by %-number% [heart[s]]]",
-			"repair %itemtypes/slots% [by %-number%]");
+			"repair %itemtypes% [by %-number%]");
 	}
 
+	@SuppressWarnings("NotNullFieldNotInitialized")
 	private Expression<?> damageables;
 	@Nullable
-	private Expression<Number> amount;
-	private boolean isHealing, isRepairing;
+	private Expression<Number> damage;
+	private boolean heal = false;
 
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({"unchecked", "null"})
 	@Override
-	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parser) {
 		if (matchedPattern == 0 && exprs[2] != null)
 			Skript.warning("The fake damage cause extension of this effect has no functionality, " +
 				"and will be removed in the future");
 
-		this.damageables = exprs[0];
-		this.isHealing = matchedPattern >= 1;
-		this.isRepairing = matchedPattern == 2;
-		this.amount = (Expression<Number>) exprs[1];
+		damageables = exprs[0];
+		if (!LivingEntity.class.isAssignableFrom(damageables.getReturnType())) {
+			if (!ChangerUtils.acceptsChange(damageables, ChangeMode.SET, ItemType.class)) {
+				Skript.error(damageables + " cannot be changed, thus it cannot be damaged or repaired.");
+				return false;
+			}
+		}
+		damage = (Expression<Number>) exprs[1];
+		heal = matchedPattern >= 1;
+
 		return true;
 	}
 
 	@Override
-	protected void execute(Event event) {
-		double amount = 0;
-		if (this.amount != null) {
-			Number amountPostCheck = this.amount.getSingle(event);
-			if (amountPostCheck == null)
+	public void execute(Event e) {
+		double damage = 0;
+		if (this.damage != null) {
+			Number number = this.damage.getSingle(e);
+			if (number == null)
 				return;
-			amount = amountPostCheck.doubleValue();
+			damage = number.doubleValue();
 		}
+		Object[] array = damageables.getArray(e);
+		Object[] newArray = new Object[array.length];
 
-		for (Object obj : this.damageables.getArray(event)) {
-			if (obj instanceof ItemType) {
-				ItemType itemType = (ItemType) obj;
+		boolean requiresChange = false;
+		for (int i = 0; i < array.length; i++) {
+			Object value = array[i];
+			if (value instanceof ItemType) {
+				ItemType itemType = (ItemType) value;
+				ItemStack itemStack = itemType.getRandom();
 
-				if (this.amount == null) {
-					ItemUtils.setDamage(itemType, 0);
-				} else {
-					ItemUtils.setDamage(itemType, (int) Math2.fit(0, (ItemUtils.getDamage(itemType) + (isHealing ? -amount : amount)), itemType.getMaterial().getMaxDurability()));
-				}
-
-			} else if (obj instanceof Slot) {
-				Slot slot = (Slot) obj;
-				ItemStack itemStack = slot.getItem();
-
-				if (itemStack == null)
-					continue;
-
-				if (this.amount == null) {
+				if (this.damage == null) {
 					ItemUtils.setDamage(itemStack, 0);
 				} else {
-					int damageAmt = (int) Math2.fit(0, (isHealing ? -amount : amount), itemStack.getType().getMaxDurability());
-					ItemUtils.setDamage(itemStack, damageAmt);
+					ItemUtils.setDamage(itemStack, (int) Math2.fit(0, ItemUtils.getDamage(itemStack) + (heal ? -damage : damage), itemStack.getType().getMaxDurability()));
 				}
 
-				slot.setItem(itemStack);
-
-			} else if (obj instanceof Damageable) {
-				Damageable damageable = (Damageable) obj;
-
-				if (this.amount == null) {
-					HealthUtils.heal(damageable, HealthUtils.getMaxHealth(damageable));
-				} else if (isHealing) {
-					HealthUtils.heal(damageable, amount);
+				newArray[i] = new ItemType(itemStack);
+				requiresChange = true;
+			} else {
+				LivingEntity livingEntity = (LivingEntity) value;
+				if (!heal) {
+					HealthUtils.damage(livingEntity, damage);
+				} else if (this.damage == null) {
+					HealthUtils.setHealth(livingEntity, HealthUtils.getMaxHealth(livingEntity));
 				} else {
-					HealthUtils.damage(damageable, amount);
+					HealthUtils.heal(livingEntity, damage);
 				}
 
+				newArray[i] = livingEntity;
 			}
 		}
+
+		if (requiresChange)
+			damageables.change(e, newArray, ChangeMode.SET);
 	}
 
 	@Override
-	public String toString(@Nullable Event event, boolean debug) {
-
-		String prefix = "damage ";
-		if (isRepairing) {
-			prefix = "repair ";
-		} else if (isHealing) {
-			prefix = "heal ";
-		}
-
-		return prefix + damageables.toString(event, debug) + (amount != null ? " by " + amount.toString(event, debug) : "");
+	public String toString(@Nullable Event e, boolean debug) {
+		return (heal ? "heal" : "damage") + " " + damageables.toString(e, debug) + (damage != null ? " by " + damage.toString(e, debug) : "");
 	}
 
 }
